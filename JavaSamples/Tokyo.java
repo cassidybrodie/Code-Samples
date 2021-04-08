37fdd102ff3e92583431e598dda14c99d5cdf84b
Cassidy <60491956+cassidybrodie@users.noreply.github.com>
2021-04-08 11:53:52 -0700

    Supporting files
    Supporting .java files for activities within Android Studio project

---
+++ JavaSamples/Tokyo.java
@@ -0,0 +1,13 @@
+package net.androidbootcamp.citiesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tokyo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tokyo);
+    }
+}
